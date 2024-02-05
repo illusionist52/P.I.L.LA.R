0a9f9dd9b598e27b30a78a7cba193664b04d0278
usmaan <2021.mohdusmaan.mogal@ves.ac.in>
2024-02-05 14:27:32 +0530

    Navbar and hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <h1>Hello World, from P.I.L.L.A.R</h1>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
