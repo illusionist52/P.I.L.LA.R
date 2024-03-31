5b18d6dfcd55cb2374da82111aed3f02bfd15ae5
usmaan <2021.mohdusmaan.mogal@ves.ac.in>
2024-03-31 20:53:15 +0530

    added footer

--- app/page.js
+++ app/page.js
@@ -1,17 +1,17 @@
 import { Features, Hero, Navbar, Steps } from "@/components";
 import FileUpload from "@/components/FileUpload";
+import Footer from "@/components/Footer";
 import React from "react";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <div className="bg-blue-200 pb-20">
+      
         <Hero />
         <Features />
-        <Steps/>
-        <FileUpload/>
-      </div>
+        <Footer/>
+     
     </div>
   );
 };
