5bd015284b7e7b3f36bb9cc7a7bbb30271742fc9
usmaan <2021.mohdusmaan.mogal@ves.ac.in>
2024-04-03 03:22:06 +0530

    merge conflict solved

--- app/glossary/page.jsx
+++ app/glossary/page.jsx
@@ -4,8 +4,53 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-        <Navbar />
-        <h1 className='flex items-center justify-center mt-10 text-3xl'>GLOSSARY</h1>
+      {/* NAVBAR */}
+      <Navbar />
+
+      <h1 className="px-4 mt-4 block text-center text-3xl font-bold md:hidden">
+        Get to know your Document
+      </h1>
+
+      {/* SITE CONTENT */}
+      <div className="flex px-4 space-y-4 flex-col justify-center my-10 md:px-0 md:flex-row md:space-x-14 md:space-y-0">
+        {/* LEFT PART HERE */}
+        <div className="md:w-[55%] bg-blue-200 border-2 border-blue-400 p-2 h-[50vh] md:h-[90vh] hide-scrollbar overflow-y-auto">
+          <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.js">
+            {viewPDF ? (
+              <Viewer fileUrl={viewPDF} />
+            ) : (
+              <div className="flex justify-center items-center h-[100%] text-4xl font-bold">
+                NO PDF
+              </div>
+            )}
+          </Worker>
+        </div>
+
+        {/* RIGTH PART HERE */}
+        <div className="md:w-[35%]  rounded-xl bg-gray-900 text-white border-2 md:h-[90vh] md:overflow-y-auto hide-scrollbar  border-blue-700 px-2 py-6 md:px-10">
+          <h1 className="text-center font-bold mb-6 text-2xl">
+            Upload Your File here
+          </h1>
+
+          <form onSubmit={handleSubmit} className="flex flex-col space-y-6">
+            <input
+              type="file"
+              onChange={handleFileChange}
+              className="text-xl"
+            />
+            <button
+              className="text-xl bg-blue-700 mx-auto text-white font-semibold w-[40%] py-2 inline-block rounded-xl"
+              onClick={handleUpload}
+              disabled={!file}
+            >
+              Upload
+            </button>
+          </form>
+
+          <hr className="my-4 border-slate-300" />
+          {showDetails && <PdfDetails isLoading={isLoading} data={data} />}
+        </div>
+      </div>
     </div>
   )
 }
--- components/FileUpload.jsx
+++ components/FileUpload.jsx
@@ -4,47 +4,68 @@ import React, { useState } from "react";
 
 const FileUpload = () => {
   const [file, setFile] = useState(null);
-  
+  const [query, setQuery] = useState("");
+  const [results,setResults] = useState(null)
   const handleFileChange = (e) => {
     setFile(e.target.files[0]);
   };
 
-  const handleUpload = async () => {
+  // const handleUpload = async () => {
+  //   try {
+  //     const formData = new FormData();
+  //     formData.append("file", file);
+
+  //     console.log("FORMDATA - ", formData);
+  //     console.log("FILE - ", file);
+
+  //     const response = await fetch("http://127.0.0.1:9999/upload", {
+  //       method: "POST",
+  //       body: formData,
+  //     });
+
+  //     if (!response.ok) {
+  //       throw new Error("Failed to upload file");
+  //     }
+
+  //     alert("File uploaded successfully");
+  //     const output = await response.json();
+  //     console.log(output);
+    
+  //   } catch (error) {
+  //     console.error("Error uploading file:", error);
+  //     alert("Error uploading file");
+  //   }
+  // };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     try {
-      setShowDetails(true)
-      setIsLoading(true)
-      const formData = new FormData();
-      formData.append("file", file);
-
-      console.log("FORMDATA - ", formData);
-      console.log("FILE - ", file);
-
-      const response = await fetch("http://127.0.0.1:5000/upload", {
-        method: "POST",
-        body: formData,
+      const response = await fetch('http://127.0.0.1:5000/cases', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ query })
       });
-
-      if (!response.ok) {
-        throw new Error("Failed to upload file");
-      }
-
-      alert("File uploaded successfully");
-      const output = await response.json();
-      console.log(output);
-      setIsLoading(false)
+      const data = await response.json();
+      console.log(data)
+      // setResults(data);
     } catch (error) {
-      console.error("Error uploading file:", error);
-      alert("Error uploading file");
+      console.error('Error fetching data:', error);
     }
   };
+
 
   return (
     <div>
       <h2>File Upload</h2>
-      <input type="file" onChange={handleFileChange} />
+      {/* <input type="file" onChange={handleFileChange} />
       <button onClick={handleUpload} disabled={!file}>
         Upload
-      </button>
+      </button> */}
+      <form onSubmit={handleSubmit}>
+       <input type="text" value={query} onChange={(e) => setQuery(e.target.value)} />
+        <button type="submit">Search</button>
+        </form>
     </div>
   );
 };
