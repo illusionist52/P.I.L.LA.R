61e80a42f4da56e04e1f04d47434b681eabf228f
Nilanchal <nilanchalpanda2003@gmail.com>
2024-04-02 13:24:14 +0530

    ui

--- app/glossary/page.jsx
+++ app/glossary/page.jsx
@@ -1,12 +1,10 @@
 "use client";
 
 import { Navbar } from "@/components";
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 import { PulseLoader } from "react-spinners";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-
-
 
 function PDFViewer() {
   const [file, setFile] = useState(null);
@@ -35,8 +33,7 @@ function PDFViewer() {
       alert("File uploaded successfully");
       const output = await response.json();
       console.log(output);
-      setData(output)
-      
+      setData(output);
     } catch (error) {
       console.error("Error uploading file:", error);
       alert("Error uploading file");
@@ -44,14 +41,32 @@ function PDFViewer() {
   };
   return (
     <div>
-       <input type="file" onChange={handleFileChange} />
-      <button onClick={handleUpload} disabled={!file}>
-        Upload
-      </button>
-      {data !== null && <p>{data.summary.output_text}</p>}
+      {/* NAVBAR */}
+      <Navbar />
 
+      <h1 className="px-4 mt-4 block text-center text-3xl font-bold md:hidden">Get to know your Document</h1>
+
+      {/* SITE CONTENT */}
+      <div className="flex px-4 space-y-4 flex-col justify-center my-10 md:px-0 md:flex-row md:space-x-14 md:space-y-0">
+        {/* LEFT PART HERE */}
+        <div className="md:w-[55%] bg-blue-200">
+          <h1>PDF PREVIEW</h1>
+          <div>
+            <h1>hello there</h1>
+          </div>
+        </div>
+
+        {/* RIGTH PART HERE */}
+        <div className="md:w-[35%] bg-red-300">
+          <input type="file" onChange={handleFileChange} />
+          <button onClick={handleUpload} disabled={!file}>
+            Upload
+          </button>
+          {data !== null && <p>{data.summary.output_text}</p>}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default PDFViewer
+export default PDFViewer;
